37361c33e090ee6ade7834a6cdd1cf059e0fa781
shimdokite <shimdokite@gmail.com>
2025-10-31 14:35:07 +0900

    feat: time props로 변경

--- app/components/GameOver.tsx
+++ app/components/GameOver.tsx
@@ -1,15 +1,16 @@
 interface GameOver {
+  time: string;
   score: number;
   start: () => void;
 }
 
-export default function GameOver({ score, start }: GameOver) {
+export default function GameOver({ time, score, start }: GameOver) {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center gap-5 text-white bg-[#42464830] rounded-md">
       <h1 className="text-3xl">Game Over!</h1>
 
       <div className="flex flex-col justify-center items-center text-xl">
-        <div className="">Time: 20:53</div>
+        <div className="">Time: {time}</div>
         <div className="w-full mr-4">Score: {score}</div>
       </div>
 
