b1abb23d4eb44b164773663133b0bffb08ac6697
shimdokite <shimdokite@gmail.com>
2025-10-31 13:51:51 +0900

    feat: 게임 오버 추가

---
+++ app/components/GameOver.tsx
@@ -0,0 +1,21 @@
+interface GameOver {
+  score: number;
+  start: () => void;
+}
+
+export default function GameOver({ score, start }: GameOver) {
+  return (
+    <div className="w-full h-full flex flex-col justify-center items-center gap-5 text-white bg-[#42464830] rounded-md">
+      <h1 className="text-3xl">Game Over!</h1>
+
+      <div className="flex flex-col justify-center items-center text-xl">
+        <div className="">Time: 20:53</div>
+        <div className="w-full mr-4">Score: {score}</div>
+      </div>
+
+      <button onClick={start} className="text-lg">
+        restart
+      </button>
+    </div>
+  );
+}
